cd4901b00b86d7311105b29c332288f06ea29d9d
ArtemEvgTitov <>
2022-09-05 18:48:50 +0300

    Добавлено решение для четвёртой задачи

--- Seminar_1/Task_4.py
+++ Seminar_1/Task_4.py
@@ -1,1 +1,16 @@
 # Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+quarter = int(input('Введите номер четверти: '))
+
+if quarter < 1 or quarter > 4:
+    print('Такой четверти не существует')
+    exit()
+elif quarter == 1:
+    range = '(x > 0; y > 0)'
+elif quarter == 2:
+    range = '(x < 0; y > 0)'
+elif quarter == 3:
+    range = '(x < 0; y < 0)'
+elif quarter == 4:
+    range = '(x > 0; y < 0)'
+print(f'Диапозон возможных точек для {quarter} четверти {range}')
