b5aa41e1f1a15097438c78a0d3b65c768012817b
ArtemEvgTitov <>
2022-09-11 19:24:43 +0300

    добавлено решение третьей задачи

--- Seminar_2/task3.py
+++ Seminar_2/task3.py
@@ -4,3 +4,21 @@
 # Это происходит до тех пор, пока не будет найден палиндром.
 # Напишите такую программу, которая найдет палиндром введенного пользователем числа.
 
+result = False
+palin_list = list(input('Введите число '))
+inverted_list = list(reversed(palin_list))
+
+while result == False:
+    if palin_list == inverted_list:
+        print(f'Палиндром = {palindrome}')
+        result = True
+    else:
+        palindrome = "".join(palin_list)
+        inverted = "".join(inverted_list)
+        palindrome = str(int(palindrome) + int(inverted))
+        palin_list = list(palindrome)
+        inverted_list = list(reversed(palin_list))
+
+
+
+
