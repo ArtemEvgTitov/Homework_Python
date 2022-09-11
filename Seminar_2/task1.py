06bef25ae0b3b8d57f79c3cc9b17a97b81d88991
ArtemEvgTitov <>
2022-09-11 17:31:09 +0300

    добавлено решение первой задачи

--- Seminar_2/task1.py
+++ Seminar_2/task1.py
@@ -4,3 +4,10 @@
 # 67.82 -> 23
 # 0.56 -> 11
 
+number = list(input('Введите число: '))
+number_list = list(range(0, 10))
+sum_num = 0
+for i in number:
+    if i in str(number_list):
+        sum_num += int(i)
+print(sum_num)
