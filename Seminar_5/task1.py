8331a65a616e17d65d9b2da20a12f0136f335da1
ArtemEvgTitov <>
2022-09-19 19:50:51 +0300

    решена первая задача

--- Seminar_5/task1.py
+++ Seminar_5/task1.py
@@ -1,2 +1,12 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 # 'абвгдейка - это передача' = >" - это передача"
+
+string_text = 'абвгдейка - это передача'
+list_string = string_text.split()
+for string in list_string:
+    if 'абв' in string:
+        list_string.remove(string)
+string_total = ''
+for string in list_string:
+    string_total += string + ' '
+print(string_total)
