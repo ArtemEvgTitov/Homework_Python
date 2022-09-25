68801f2732536d3b22f83097f2b7ede424a7fece
ArtemEvgTitov <>
2022-09-25 21:31:10 +0300

    Решены основные 6 задач дз

--- Seminar_6/Tasks1_6.py
+++ Seminar_6/Tasks1_6.py
@@ -15,24 +15,45 @@
 # 1
 print('\nTask 1')
 
+list_task1 = ['sdf', 'werwe', '2', 'rewq']
+list_task1 = True in [n.isdigit() for n in list_task1]
+print(list_task1)
+
 # 2
 print('\nTask 2')
 
 list_task2 = [2, 3, 5, 9, 3, 5, 7, 3, 9, 2]  # 3 9 5 3 2 сумма 22
-new_list = list(filter(lambda x: x[0] % 2 != 0, enumerate(list_task2)))
-print(sum([n[1] for n in new_list]))
+list_task2 = list(filter(lambda x: x[0] % 2 != 0, enumerate(list_task2)))
+print(sum([n[1] for n in list_task2]))
 
 # 3
 print('\nTask 3')
+import math
+
+numbers = '5 1 2 1'
+list_task3 = [float(n) for n in list(numbers.replace(" ", ""))]
+sq = lambda x2, x1: (x2-x1)**2
+print(round(math.sqrt((sq(list_task3[2], list_task3[0]) + sq(list_task3[3], list_task3[1]))), 2))
 
 # 4
 print('\nTask 4')
 
+st = 2
+list_task4 = ["qwe", "asd", "zxc", "qwe", "ertqwe"]
+try:
+    list_task4 = list(filter(lambda x: x, enumerate(list_task4)))
+    temp = [n[0] for n in list_task4 if n[1] == st]
+    print(temp[1])
+except IndexError:
+    print(-1)
+
 # 5
 print('\nTask 5')
 
-list_task5 = [2, 3, 4, 5, 6] # [12, 15, 16]
-temp = list(map(lambda x, y: x * y, list(reversed(list_task5)), list_task5[:-2]))
+list_task5 = [2, 3, 4, 5, 6]  # [12, 15, 16]
+size = int(len(list_task5) / 2)
+temp = list(
+    map(lambda x, y: x * y, list(reversed(list_task5)), list_task5[:-size]))
 print(temp)
 
 # 6
