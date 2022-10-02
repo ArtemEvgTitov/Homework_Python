b193d9a708338274fbd495e894de71eac93bf12b
ArtemEvgTitov <>
2022-10-03 00:12:11 +0300

    Добавил логирование в check

--- Seminar_9/Calculator/check.py
+++ Seminar_9/Calculator/check.py
@@ -1,8 +1,13 @@
+import logger as log
+
+
 def check_number(text):
     try:
         a = float(text[0])
         b = float(text[1])
     except ValueError:
+        log.log_two_argument('Ошибка при вводе чисел',
+                             f'Пользователь ввёл: {text}')
         a, b = list(text[0]), list(text[1])
         for index, i in enumerate(a):
             if i.isdigit() == False:
@@ -12,4 +17,5 @@ def check_number(text):
                 b[index] = '.'
         a = float(''.join(a))
         b = float(''.join(b))
+        log.log_one_argument(f'Исправил на: {a} и {b}')
     return a, b
