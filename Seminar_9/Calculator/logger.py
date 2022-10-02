68237e5f4773c2b63de93c2fc807d3eb5c5116d5
ArtemEvgTitov <>
2022-10-02 18:57:25 +0300

    Упрощён логгер

--- Seminar_9/Calculator/logger.py
+++ Seminar_9/Calculator/logger.py
@@ -1,22 +1,15 @@
 from datetime import datetime as dt
 
-def log_to_file(arg1, arg2, operation, result):
+def log_one_argument(text):
     path = 'logging.csv'
     time_sign = dt.now().strftime('%D %H:%M')
     f = open(path, 'a')
-    f.write(f'{time_sign}--> {arg1} {operation} {arg2} = {result}\n')
-    f.close()
-
-def log_exit():
-    path = 'logging.csv'
-    time_sign = dt.now().strftime('%D %H:%M')
-    f = open(path, 'a', encoding="utf-8")
-    f.write(f'{time_sign}--> Отмена операции\n')
-    f.close()
-
-def log_error(text):
-    path = 'logging.csv'
-    time_sign = dt.now().strftime('%D %H:%M')
-    f = open(path, 'a', encoding="utf-8")
     f.write(f'{time_sign}--> {text}\n')
+    f.close()
+
+def log_two_argument(text1, text2):
+    path = 'logging.csv'
+    time_sign = dt.now().strftime('%D %H:%M')
+    f = open(path, 'a', encoding="utf-8")
+    f.write(f'{time_sign}--> {text1} - {text2}\n')
     f.close()
