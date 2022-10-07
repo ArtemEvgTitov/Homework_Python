c1f5e2bb46c82672590566eed4170c82ff889c6a
ArtemEvgTitov <>
2022-10-07 22:31:53 +0300

    recording готов без проверок

--- Seminar_10/recording.py
+++ Seminar_10/recording.py
@@ -1,6 +1,5 @@
 import logger as log
 from settings import TOKEN
-import random
 import json
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import (
@@ -11,17 +10,48 @@ from telegram.ext import (
     ConversationHandler,
 )
 
+tel_numb = {"id": 0, "surname": '', "name": '', "tel": '', "coment": ''}
+contact = []
+
 
 def save():
-    global tel_numb
-    with open("tel_numb.json", "w", encoding="utf-8") as reg:
-        reg.write(json.dumps(tel_numb, ensure_ascii=False))
+    with open("tel_numb.json", "w", encoding="utf-8") as tel:
+        tel.write(json.dumps(tel_numb, ensure_ascii=False))
+
+
+def load():
+    global contact
+    with open("contacts.json", "r", encoding="utf-8") as cont:
+        contact = json.load(cont)
+
+
+def add_contact(tel_numb):
+    try:
+        load()
+        temp = []
+        ID = False
+        for i in contact:
+            temp.append(i["id"])
+        while ID == False:
+            if tel_numb["id"] in temp:
+                tel_numb["id"] += 1
+            else:
+                ID = True
+        contact.append(tel_numb)
+        with open("contacts.json", "w", encoding="utf-8") as cont:
+            cont.write(json.dumps(contact, ensure_ascii=False))
+        log.text_in_log(f"В телефонную книгу добавлен контакт: \n+++ {tel_numb['surname']} {tel_numb['name']} тел. {tel_numb['tel']}")
+    except:
+        contact.append(tel_numb)
+        with open("contacts.json", "w", encoding="utf-8") as cont:
+            cont.write(json.dumps(contact, ensure_ascii=False))
+        log.text_in_log(f"В телефонную книгу добавлен контакт: \n+++ {tel_numb['surname']} {tel_numb['name']} тел. {tel_numb['tel']}")
 
 
 SURNAME, NAME, TEL, COMMENT = range(4)
 
 
-def start(update, _):
+def record(update, _):
     log.text_in_log("Запись контакта")
     update.message.reply_text('Для отмены введите /cancel\n\nВведите фамилию')
     return SURNAME
@@ -36,20 +66,36 @@ def cancel(update, _):
     return ConversationHandler.END
 
 
-def surname():
-    pass
+def surname(update, _):
+    tel_numb['surname'] = update.message.text
+    save()
+    log.text_in_log(f"Введена фамилия - {update.message.text}")
+    update.message.reply_text('Введите имя')
+    return NAME
 
 
-def name():
-    pass
+def name(update, _):
+    tel_numb['name'] = update.message.text
+    save()
+    log.text_in_log(f"Введено имя - {update.message.text}")
+    update.message.reply_text('Введите номер телефона')
+    return TEL
 
 
-def tel():
-    pass
+def tel(update, _):
+    tel_numb['tel'] = update.message.text
+    save()
+    log.text_in_log(f"Введён номер телефона - {update.message.text}")
+    update.message.reply_text('Введите коментарий к контакту')
+    return COMMENT
 
 
-def comment():
-    pass
+def comment(update, _):
+    tel_numb['coment'] = update.message.text
+    save()
+    log.text_in_log(f"Введён коментарий - {update.message.text}")
+    add_contact(tel_numb)
+    return ConversationHandler.END
 
 
 if __name__ == '__main__':
@@ -57,7 +103,7 @@ if __name__ == '__main__':
     dispatcher = updater.dispatcher
 
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('record', start)],
+        entry_points=[CommandHandler('record', record)],
         states={
             SURNAME: [MessageHandler(Filters.text & ~Filters.command, surname)],
             NAME: [MessageHandler(Filters.text & ~Filters.command, name)],
