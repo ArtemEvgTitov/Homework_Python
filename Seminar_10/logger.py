c6b54da6cff192a20d6cf9507d25e04aa15789fd
ArtemEvgTitov <>
2022-10-07 17:43:22 +0300

    Исправлен логгер

--- Seminar_10/logger.py
+++ Seminar_10/logger.py
@@ -2,7 +2,7 @@ from datetime import datetime as dt
 
 
 def text_in_log(text):
-    path = 'log_calc.csv'
+    path = 'log_10_seminar.csv'
     time_sign = dt.now().strftime('%D %H:%M')
     f = open(path, 'a', encoding="utf-8")
     f.write(f'{time_sign}--> {text}\n')
